d9c79a0db7fa6ba3d447e2aff53bc471e0c516fc
1ByteBit <1bitbyte@duck.com>
2023-04-10 20:37:52 -0400

    Initial commit

---
+++ src/main/java/com/github/bitbyte/yetanotherrtp/YetAnotherRTP.java
@@ -0,0 +1,17 @@
+package com.github.bitbyte.yetanotherrtp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class YetAnotherRTP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
